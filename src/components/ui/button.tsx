22c534023c29d280e71031d229aeee7188fe8a7f
Ryder090 <ridergautam2121@gmail.com>
2026-05-25 17:59:18 +0530

    chore: finalize responsive audit experience and deployment polish. Final Deployment

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -2,6 +2,7 @@ import * as React from "react"
 import { Slot } from "@radix-ui/react-slot"
 import { cva, type VariantProps } from "class-variance-authority"
 
+
 import { cn } from "@/lib/utils"
 
 const buttonVariants = cva(
@@ -40,7 +41,7 @@ const buttonVariants = cva(
 
 export interface ButtonProps
   extends React.ButtonHTMLAttributes<HTMLButtonElement>,
-    VariantProps<typeof buttonVariants> {
+  VariantProps<typeof buttonVariants> {
   asChild?: boolean
 }
 
